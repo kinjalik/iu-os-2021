74b1f00cbf9f6f0d291006a8fbfd8a2279545a68
Albert Akmukhametov <albert@kinjalik.ru>
2021-09-15 13:37:08 +0300

    Ex2 done

--- week05/ex2.sh
+++ week05/ex2.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+
+while test -f "file.lock" ; do
+    sleep 1
+done
+
+ln file file.lock
+
 i="$(cat file | awk '{print $NF}')"
 ((i+=1))
 
 echo "$(cat file) $i" > file
+
+rm file.lock
