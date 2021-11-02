8d1207912b08e589813b80ca54719d8291e4a391
Albert Akmukhametov <albert@kinjalik.ru>
2021-11-02 20:17:53 +0300

    ex1

---
+++ week11/ex1.sh
@@ -0,0 +1,10 @@
+cat /dev/urandom > lofs.img &
+sleep 1
+kill $(jobs -p)
+
+sudo losetup -f lofs.img
+
+sudo mkfs.ext4 $(losetup -a | grep 'lofs.img' | awk '{print $1}' | cut -d ':' -f 1)
+
+sudo mount $(losetup -a | grep 'lofs.img' | awk '{print $1}' | cut -d ':' -f 1) ./lofsdisk
+
